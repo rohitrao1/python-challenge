9bab738268f13e9e055a6b8dc03ecc5705871893
Rohit Rao <ro.rao2007@gmail.com>
2018-08-22 15:07:14 -0400

    git commit

---
+++ PyPoll/main.py
@@ -0,0 +1,80 @@
+import os
+import csv
+
+election_data = os.path.join("election_data.csv")
+
+
+#Method 1 --> Dictionaries
+total_votes = 0
+voting_dict = {"Khan":[0,0.0], "Correy":[0,0.0], "Li": [0,0.0], "O-Tooley":[0,0.0]}
+
+with open(election_data, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+
+    csv_header = next(csvreader)
+    
+    for row in csvreader:
+        total_votes += 1
+        if row[2] == "Khan":
+            voting_dict["Khan"][0] += 1
+        elif row[2] == "Correy":
+            voting_dict["Correy"][0] += 1
+        elif row[2] == "Li":
+            voting_dict["Li"][0] += 1
+        else:
+            voting_dict["O-Tooley"][0] += 1
+
+voting_dict["Khan"][1] = voting_dict["Khan"][0]/total_votes
+voting_dict["Correy"][1] = voting_dict["Correy"][0]/total_votes
+voting_dict["Li"][1] = voting_dict["Li"][0]/total_votes
+voting_dict["O-Tooley"][1] = voting_dict["O-Tooley"][0]/total_votes
+
+print(voting_dict)
+
+'''
+Things To Do:
+1. Should the names of the candidates (keys of the dictionary) be extracted 
+automatically?
+2. Print & format output properly 
+3. Export as .txt
+4. "Verify" it works for other file (with a 0 output)
+'''
+
+
+'''
+#Method 2 --> Manual calculations 
+
+total_votes = 0
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
+percent_khan = 0.0
+percent_correy = 0.0
+percent_li = 0.0
+percent_otooley = 0.0
+
+
+with open(election_data, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+
+    csv_header = next(csvreader)
+    for row in csvreader:
+        total_votes += 1
+        if row[2] == "Khan":
+            khan_votes += 1
+        elif row[2] == "Correy":
+            correy_votes += 1
+        elif row[2] == "Li":
+            li_votes += 1
+        else:
+            otooley_votes += 1
+    percent_khan = float(khan_votes/total_votes)
+    percent_correy = float(correy_votes/total_votes)
+    percent_li = float(li_votes/total_votes)
+    percent_otooley = float(otooley_votes/total_votes)
+
+print(total_votes)
+print(khan_votes)
+print(percent_khan)
+'''
