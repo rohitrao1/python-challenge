598dab1883c02b3c0c2c23b1af41281c5d16030d
Rohit Rao <ro.rao2007@gmail.com>
2018-08-24 20:40:17 -0400

    git commit "Fixed format"

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -41,6 +41,7 @@ with open(election_data, newline = "") as csvfile:
     for votes in num_votes:
         percentage = (votes/total_votes) * 100
         percentage = round(percentage)
+        percentage = "%.3f%%" % percentage
         percent_votes.append(percentage)
     
     # Find the winning candidate
@@ -50,26 +51,26 @@ with open(election_data, newline = "") as csvfile:
 
 # Displaying results
 print("Election Results")
-print("---------------------")
+print("--------------------------")
 print(f"Total Votes: {str(total_votes)}")
-print("---------------------")
+print("--------------------------")
 for i in range(len(candidates)):
     print(f"{candidates[i]}: {str(percent_votes[i])} ({str(num_votes[i])})")
-print("---------------------")
+print("--------------------------")
 print(f"Winner: {winning_candidate}")
-print("---------------------")
+print("--------------------------")
 
 # Exporting to .txt file
 output = open("output.txt", "w")
 line1 = "Election Results"
-line2 = "---------------------"
+line2 = "--------------------------"
 line3 = str(f"Total Votes: {str(total_votes)}")
-line4 = str("---------------------")
+line4 = str("--------------------------")
 output.write('{}\n{}\n{}\n{}\n'.format(line1, line2, line3, line4))
 for i in range(len(candidates)):
     line = str(f"{candidates[i]}: {str(percent_votes[i])} ({str(num_votes[i])})")
     output.write('{}\n'.format(line))
-line5 = "---------------------"
+line5 = "--------------------------"
 line6 = str(f"Winner: {winning_candidate}")
-line7 = "---------------------"
+line7 = "--------------------------"
 output.write('{}\n{}\n{}\n'.format(line5, line6, line7))
